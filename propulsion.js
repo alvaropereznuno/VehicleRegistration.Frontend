00daca4f3123575243b60428af4ee6309fc906cf
USERSAD\aperenun <aperenun@emeal.nttdata.com>
2025-09-03 23:35:01 +0200

    Se arreglan problemas en los gráficos y se terminan los de propulsión.

--- propulsion.js
+++ propulsion.js
@@ -5,19 +5,17 @@ import SharedUtils from './utils/sharedUtils.js';
 const propulsion = {
     initialize: async function (){
         ChartUtils.propulsion.motorTypesAcc.create(SharedUtils.data.registrationFilteredList, $('#motorTypesAcc'));
-        ChartUtils.propulsion.motorTypesAcc100.create(SharedUtils.data.registrationFilteredList, $('#motorTypesAcc100'));
-        ChartUtils.propulsion.motorTypesPie.create(SharedUtils.data.registrationFilteredList, $('#motorTypesPie'));
+        ChartUtils.propulsion.motorTypes100.create(SharedUtils.data.registrationFilteredList, $('#motorTypes100'));
         ChartUtils.propulsion.motorTypesAnnualDiff.create(SharedUtils.data.registrationFilteredList, $('#motorTypesAnnualDiff'));
-        // ChartUtils.propulsion.motorTypesAnnualDiff100.create(SharedUtils.data.registrationFilteredList, $('#motorTypesAnnualDiff100'));
+        ChartUtils.propulsion.motorTypesAnnualDiff100.create(SharedUtils.data.registrationFilteredList, $('#motorTypesAnnualDiff100'));
 
         // GridUtils.ranking.topResults.create(SharedUtils.data.registrationFilteredList, document.getElementById('topResults'));
 
         window.addEventListener("globalDataUpdated", () => {
              ChartUtils.propulsion.motorTypesAcc.update(SharedUtils.data.registrationFilteredList);
-             ChartUtils.propulsion.motorTypesAcc100.update(SharedUtils.data.registrationFilteredList);
-             ChartUtils.propulsion.motorTypesPie.update(SharedUtils.data.registrationFilteredList);
+             ChartUtils.propulsion.motorTypes100.update(SharedUtils.data.registrationFilteredList);
              ChartUtils.propulsion.motorTypesAnnualDiff.update(SharedUtils.data.registrationFilteredList);
-             // ChartUtils.propulsion.motorTypesAnnualDiff100.update(SharedUtils.data.registrationFilteredList100);
+             ChartUtils.propulsion.motorTypesAnnualDiff100.update(SharedUtils.data.registrationFilteredList100);
      
              // GridUtils.ranking.topResults.update(SharedUtils.data.registrationFilteredList);
          });
